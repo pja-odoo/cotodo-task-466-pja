b4185f52b3c168e4181389b50a851cc6be224b56
Henil Malaviya <henilm.dev@gmail.com>
2024-06-29 10:14:14 +0530

    feat: auth button

---
+++ src/lib/components/AuthButton.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import type { AuthProvider } from '$lib/types/auth';
+	import { Button } from '$components/ui/button';
+	import { useAuth } from '$lib/use/auth';
+	import Icon from '@iconify/svelte';
+
+	export let icon: string;
+	export let provider: AuthProvider;
+</script>
+
+<Button href={useAuth.getAuthorizationURL(provider).href} class="gap-2" size="lg">
+	<Icon font-size={23} {icon} />
+	<span>
+		Continue with <span class="capitalize">{provider}</span>
+	</span>
+</Button>
