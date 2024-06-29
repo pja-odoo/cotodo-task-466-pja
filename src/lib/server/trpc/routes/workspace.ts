539be0890c07bd6b04e7dae312fed43b60923f0c
Henil Malaviya <henilm.dev@gmail.com>
2024-06-29 12:44:07 +0530

    feat(tRPC): basic CRUD operations of workspace

---
+++ src/lib/server/trpc/routes/workspace.ts
@@ -0,0 +1,106 @@
+import { db } from '$lib/server/db';
+import { dbService } from '$lib/server/services/db';
+import { tryCatch } from '$lib/utils/tryCatch';
+import { privateProcedure, publicProcedure, router } from '..';
+import { z } from 'zod';
+
+export const workspaceRoute = router({
+	create: privateProcedure
+		.input(
+			z.object({
+				name: z.string()
+			})
+		)
+		.mutation(async ({ ctx, input }) => {
+			const [data, error] = await tryCatch(() => {
+				return dbService.workspace.createWorkspace({
+					name: input.name,
+					userId: ctx.user.id!
+				});
+			});
+
+			if (error) {
+				return {
+					error: true,
+					code: 'DATABASE_ERROR',
+					message: 'Failed to create workspace'
+				};
+			}
+
+			return {
+				error: false,
+				code: 'DONE',
+				message: 'Workspace created',
+				data: {
+					id: data.id,
+					name: data.name
+				}
+			};
+		}),
+
+	update: privateProcedure
+		.input(
+			z.object({
+				id: z.string(),
+				values: z.object({
+					name: z.string().optional()
+				})
+			})
+		)
+		.mutation(async ({ ctx, input }) => {
+			const [data, error] = await tryCatch(() => {
+				return dbService.workspace.updateWorkspace({
+					id: input.id,
+					values: input.values
+				});
+			});
+
+			if (error) {
+				return {
+					error: true,
+					code: 'DATABASE_ERROR',
+					message: 'Failed to create workspace'
+				};
+			}
+
+			return {
+				error: false,
+				code: 'DONE',
+				message: 'Workspace updated',
+				data: {
+					id: data.id,
+					name: data.name
+				}
+			};
+		}),
+
+	deleteWorkSpace: privateProcedure
+		.input(
+			z.object({
+				id: z.string()
+			})
+		)
+		.mutation(async ({ ctx, input }) => {
+			const [data, error] = await tryCatch(() => {
+				return db.workspace.delete({
+					where: {
+						id: input.id
+					}
+				});
+			});
+
+			if (error) {
+				return {
+					error: true,
+					code: 'DATABASE_ERROR',
+					message: 'Failed to delete workspace'
+				};
+			}
+
+			return {
+				error: false,
+				code: 'DONE',
+				message: 'Workspace deleted'
+			};
+		})
+});
